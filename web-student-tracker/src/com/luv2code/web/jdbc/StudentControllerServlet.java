5eea5c947be116072b0cb5e385289b2ef741767b
Israel Mesa <mesa.israel50@gmail.com>
2018-06-27 23:49:22 -0400

    Delete Student - Developing the Servlet and JDBC Code

--- web-student-tracker/src/com/luv2code/web/jdbc/StudentControllerServlet.java
+++ web-student-tracker/src/com/luv2code/web/jdbc/StudentControllerServlet.java
@@ -65,6 +65,10 @@ public class StudentControllerServlet extends HttpServlet {
 					updateStudent(request, response);
 					break;
 				
+				case "DELETE":
+					deleteStudent(request, response);
+					break;
+				
 				default:
 					listStudents(request, response);
 					break;
@@ -76,6 +80,17 @@ public class StudentControllerServlet extends HttpServlet {
 		catch (Exception exc) {
 			throw new ServletException(exc);
 		}
+	}
+
+	private void deleteStudent(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		//read the student id from the form data
+		String theStudentId = request.getParameter("studentId");
+		
+		//delete student from the database
+		studentDbUtil.deleteStudent(theStudentId);
+		
+		//send them back to the "list students" page
+		listStudents(request, response);
 	}
 
 	private void updateStudent(HttpServletRequest request, HttpServletResponse response) throws Exception{
